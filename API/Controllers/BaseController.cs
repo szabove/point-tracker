fb94f833c146e9b57401fece9e7b60a1b92307a8
szabove <szabove@gmail.com>
2022-10-06 18:58:41 +0200

    Initial commit
    - Created solution
    - Added class library projects
    - Added .gitignore file
    - Added BaseController class

---
+++ API/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class BaseController : Controller
+    {
+    }
+}
