f96dac5457cf89e555d67c650daa91e991f3e024
szabove <szabove@gmail.com>
2022-10-09 16:57:57 +0200

    Added Domain entities

---
+++ Domain/Model/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Model
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string PayerName { get; set; }
+        public int Points { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
